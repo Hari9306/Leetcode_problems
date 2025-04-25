e04c6fc0d9ab7297540c3fe34380c5681512dabe
Hari9306 <hariveera9306@gmail.com>
2025-04-25 14:19:10 +0530

    Time: 191 ms (5.07%), Space: 16.3 MB (10.93%) - LeetHub

---
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        set<int>v;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size();j++){
+             if(abs(i-j)<=k&&nums[j]==key){
+              v.insert(i);
+             }
+            }
+        }
+        
+     return vector<int>(v.begin(), v.end());
+    }
+};
