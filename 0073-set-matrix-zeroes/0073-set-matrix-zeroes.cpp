a35ea5e2098743e459363b8dca6b7360325517a2
Hari9306 <hariveera9306@gmail.com>
2025-05-23 23:14:28 +0530

    Time: 8 ms (3.3%), Space: 20.3 MB (6.71%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,39 +2,37 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         vector<vector<int>> a;
-         int col=matrix[0].size();
-        int row=matrix.size();
-        for(int i=0;i<row;i++){
-            vector<int>b;
-          for(int j=0;j<col;j++){
-            b.push_back(matrix[i][j]);
-          }
-          a.push_back(b);
+        int col = matrix[0].size();
+        int row = matrix.size();
+        for (int i = 0; i < row; i++) {
+            vector<int> b;
+            for (int j = 0; j < col; j++) {
+                b.push_back(matrix[i][j]);
+            }
+            a.push_back(b);
         }
-       
-        for(int i=0;i<row;i++){
-          for(int j=0;j<col;j++){
-            if(matrix[i][j]==0){
-                int k=0;
-                while(k<col){
-                    a[i][k]=0;
-                    k++;
-                }
-                int l=0;
-                while(l<row){
-                    a[l][j]=0;
-                    l++;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    int k = 0;
+                    while (k < col) {
+                        a[i][k] = 0;
+                        k++;
+                    }
+                    int l = 0;
+                    while (l < row) {
+                        a[l][j] = 0;
+                        l++;
+                    }
                 }
             }
-          }
         }
-         for(int i=0;i<row;i++){
-          
-          for(int j=0;j<col;j++){
-           matrix[i][j]=a[i][j];
-          }
-       
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                matrix[i][j] = a[i][j];
+            }
         }
-        return ;
+        return;
     }
 };
