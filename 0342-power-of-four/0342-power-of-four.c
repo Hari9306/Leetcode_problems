5ac51e5a879c8ed21aaa5c7516707a9b96a54001
Hari9306 <hariveera9306@gmail.com>
2025-08-15 19:59:50 +0530

    Time: 0 ms (100%), Space: 7.7 MB (56.61%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.c
@@ -0,0 +1,13 @@
+bool isPowerOfFour(int n) {
+     if(n==0){
+        return 0;
+    }
+    if(n==1) return true;
+ while (n!=1){
+    if(n%4!=0){
+        return false;
+    }
+      n=n/4;
+ }   
+ return 1;
+}
