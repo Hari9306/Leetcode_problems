4ea65e13708011f552e5edecd5357097db44e721
Hari9306 <hariveera9306@gmail.com>
2025-08-28 15:46:57 +0530

    Time: 612 ms (25.33%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,5 +1,8 @@
-/* Write your PL/SQL query statement below */
-select product_id,year as first_year,quantity,price 
-from sales
-where (product_id,year)in (select product_id,min(year) from sales
-group by product_id);
+-- /* Write your PL/SQL query statement below */
+-- select product_id,year as first_year,quantity,price 
+-- from sales
+-- where (product_id,year)in (select product_id,min(year) from sales
+-- group by product_id);
+select product_id,year as first_year,quantity,price
+from sales s
+where year=(select min(year) from sales where product_id=s.product_id);
