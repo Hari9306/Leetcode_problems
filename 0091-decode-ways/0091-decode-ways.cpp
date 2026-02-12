10e3a246d09aa93a7be747954c3fd491163c05ae
Hari9306 <hariveera9306@gmail.com>
2026-02-12 11:28:05 +0530

    Time: 0 ms (100%), Space: 8.4 MB (89.03%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+      int n=s.size();
+      vector<int>dp(n,0);
+      if(s[0]!='0') dp[0]=1;
+      for(int i =1;i<n;i++){
+        if(s[i]!='0') {
+            dp[i]+=dp[i-1];
+        }
+        int two=(s[i-1]-'0')*10+(s[i]-'0');
+        if(two>=10 && two <=26){
+            if(i==1){
+            dp[i]+=1;
+            }
+            else {
+            dp[i]+=dp[i-2];
+            }
+        }
+      }
+        return dp[n-1];
+    }
+};
