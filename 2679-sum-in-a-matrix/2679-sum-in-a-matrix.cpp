d90e141424b2d0e28f9169477266e928325276bb
Hari9306 <hariveera9306@gmail.com>
2026-03-17 10:39:46 +0530

    Time: 16 ms (52.95%), Space: 53.1 MB (80.8%) - LeetHub

---
+++ 2679-sum-in-a-matrix/2679-sum-in-a-matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int matrixSum(vector<vector<int>>& nums) {
+        int n=nums.size();
+        int m=nums[0].size();
+        for(int i=0;i<n;i++){
+            sort(nums[i].begin(),nums[i].end());
+        }
+        int sum=0;
+        for(int j=0;j<m;j++){
+            int maxi=INT_MIN;
+            for(int i=0;i<n;i++){
+                maxi=max(maxi,nums[i][j]);
+            }
+            sum+=maxi;
+        }
+       return sum;
+    }
+};
