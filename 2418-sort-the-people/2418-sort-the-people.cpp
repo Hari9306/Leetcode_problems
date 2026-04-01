4f15d9a5d1ac28f9ee9f1b328dafd9f9757ed8f0
Hari9306 <hariveera9306@gmail.com>
2026-04-01 22:17:42 +0530

    Time: 8 ms (73.41%), Space: 32.2 MB (7.63%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<pair<int,string>>res;
+        for(int i=0;i<names.size();i++){
+            res.push_back({heights[i],names[i]});
+        }
+        sort(res.rbegin(),res.rend());
+        vector<string>ans;
+        for(auto it : res){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
