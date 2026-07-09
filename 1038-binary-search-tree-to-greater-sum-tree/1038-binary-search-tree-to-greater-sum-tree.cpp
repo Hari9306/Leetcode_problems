87a324c6ef43a0ea8649f3dc3f09f74be6ead097
Hari9306 <hariveera9306@gmail.com>
2026-07-09 23:11:10 +0530

    Time: 0 ms (100%), Space: 11.3 MB (79.76%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum=0;
+    void postorder(TreeNode* root){
+        if(!root) return ;
+        postorder(root->right);
+        root->val=root->val+sum;
+        sum=root->val;
+        postorder(root->left);
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        postorder(root);
+        return root;   
+    }
+};
