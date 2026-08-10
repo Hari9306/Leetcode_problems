220690341770c5cb29c0f93ac01c56ab8e4e9393
Hari9306 <hariveera9306@gmail.com>
2026-08-10 15:48:11 +0530

    Time: 3 ms (31.67%), Space: 13.5 MB (35.01%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(int i=31;i>=0;i--){
+            int cnt=0;
+            for(auto x : nums){
+                int val= x&(1 <<i);
+                if(val) cnt++;
+            }
+            if(cnt % 3 ) ans |= (1 << i );
+        }
+        return ans;
+    }
+};
