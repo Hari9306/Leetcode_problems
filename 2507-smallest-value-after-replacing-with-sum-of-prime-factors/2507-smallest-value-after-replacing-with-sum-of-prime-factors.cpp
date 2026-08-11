e7487aa12e71ca4632a9df318b184ad057c0d2c7
Hari9306 <hariveera9306@gmail.com>
2026-08-11 15:34:51 +0530

    Time: 67 ms (7.09%), Space: 102.5 MB (5.13%) - LeetHub

---
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void check(int n,vector<int> &prime){
+        prime[0]=false;
+        prime[1]=false;
+        for(int i=2;i*i <=n;i++){
+            if(prime[i]){
+                for(int j=i*i;j<=n;j+=i){
+                    prime[j]=false;
+                }
+            }
+        }
+    }
+    int smallestValue(int n) {
+        vector<int> prime(1e5+1,true);
+        check(1e5,prime);
+        int sum=0;
+        while(true){
+            int temp=n;
+            for(int i=2;i<=temp && n > 1 ;i++){
+                if(prime[i]){
+                    while(n%i==0){
+                        sum+=i;
+                        n=n/i;
+                    }
+                }
+                // cout<< sum << n << " ";
+            }
+            // cout<< sum <<n << " ";
+            if(sum==temp) return temp;
+            else{
+                n=sum;
+                sum=0;
+            }
+        }
+        return n;
+    }
+};
