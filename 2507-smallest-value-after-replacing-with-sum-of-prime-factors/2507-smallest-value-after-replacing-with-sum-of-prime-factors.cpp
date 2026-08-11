7babf10d4d0367e1c6a69c2945a56bcfcda7c5dc
Hari9306 <hariveera9306@gmail.com>
2026-08-11 15:35:44 +0530

    Time: 64 ms (7.09%), Space: 102.4 MB (5.13%) - LeetHub

--- 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
@@ -17,7 +17,7 @@ public:
         int sum=0;
         while(true){
             int temp=n;
-            for(int i=2;i<=temp && n > 1 ;i++){
+            for(int i=2;i*i<=temp && n > 1 ;i++){
                 if(prime[i]){
                     while(n%i==0){
                         sum+=i;
@@ -27,6 +27,7 @@ public:
                 // cout<< sum << n << " ";
             }
             // cout<< sum <<n << " ";
+            if(n >1 ) sum+=n;
             if(sum==temp) return temp;
             else{
                 n=sum;
