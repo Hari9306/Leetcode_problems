3fe8f863c01fad3c4a4f942fb5275082246c09fc
Hari9306 <hariveera9306@gmail.com>
2026-08-25 15:35:20 +0530

    Time: 3 ms (77.93%), Space: 39.5 MB (71.87%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.cpp
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    int prime(int num) {
-        if (num < 2) return 0;
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) return 0;
+    bool prime(int n){
+        if(n<=1) return false;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0) return false;
         }
-        return 1;
+        return true;
     }
-
     int diagonalPrime(vector<vector<int>>& nums) {
-        vector<int> v;
-        int n = nums.size();
-
-        for (int i = 0; i < n; i++) {
-            // No need for inner loop (j-loop) since we only deal with diagonals
-
-            if (prime(nums[i][i]) == 1) {
-                v.push_back(nums[i][i]);
-            }
-            if (prime(nums[i][n - i - 1]) == 1) {
-                v.push_back(nums[i][n - i - 1]);
-            }
-        }
-
-        if (!v.empty()) {
-            int max_ele = *max_element(v.begin(), v.end());
-            return max_ele;
-        }
-        return 0;
+        int n=nums.size();
+        int m=nums[0].size();
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            if(prime(nums[i][i])) maxi=max(maxi,nums[i][i]);
+            if(prime(nums[i][n-i-1])) maxi=max(maxi,nums[i][n-i-1]);
+        }   
+        return maxi;
     }
 };
