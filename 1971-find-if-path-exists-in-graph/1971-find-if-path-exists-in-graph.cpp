dd28344ea07b0a5dacdcf69a1c51b745a99da43f
Hari9306 <hariveera9306@gmail.com>
2026-08-27 09:59:18 +0530

    Time: 401 ms (35.62%), Space: 328.7 MB (26.42%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,27 +1,63 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+    bool dfs(int source,int destination,vector<vector<int>> &Adj,vector<bool> &vis){
         if(source==destination) return true;
-        vector<vector<int>> Adj(n);
-        for(int i=0;i<edges.size();i++){
-            Adj[edges[i][0]].push_back(edges[i][1]);
-            Adj[edges[i][1]].push_back(edges[i][0]);
-        }
-        vector<int> vis(n,0);
-        queue<int> q;
-        q.push(source);
-        vis[source]=1;
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            for(auto it : Adj[node]){
-                if(!vis[it]){
-                    vis[it]=1;
-                    if(it==destination) return true;
-                    q.push(it);
+        vis[source]=true;
+        for(auto x : Adj[source]){
+            if(!vis[x]) {
+                if(dfs(x,destination,Adj,vis)) {
+                    return true;
                 }
             }
         }
         return false;
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        //dfs 
+        vector<vector<int>> Adj(n);
+        for(auto x : edges){
+            Adj[x[0]].push_back(x[1]);
+            Adj[x[1]].push_back(x[0]);
+        }
+        vector<bool> vis(n,false);
+        return dfs(source,destination,Adj,vis);
+
+
+        //bfs
+        // vector<vector<int>> Adj(n);
+        // for(auto x :edges){
+        //     Adj[x[0]].push_back(x[1]);
+        //     Adj[x[1]].push_back(x[0]);
+        // }
+        // vector<bool> vis(n,false);
+        // queue<int> q;
+        // q.push(source);
+        // vis[source]=true;
+        // while(!q.empty()){
+        //     int node=q.front();
+        //     q.pop();
+        //     for(auto x : Adj[node]){
+        //         if(!vis[x]){
+        //             if(x==destination) return true;
+        //             vis[x]=true;
+        //             q.push(x);
+        //         }
+        //     }
+        // }
+        // return false;
+
+
+
+
+        //normal way 
+        // vector<vector<int>> Adj(n);
+        // for(auto x : edges){
+        //     Adj[x[0]].push_back(x[1]);
+        //     Adj[x[1]].push_back(x[0]);
+        // }
+        // for(auto x : Adj[source]){
+        //     if(x==destination) return true;
+        // }
+        // return false;
     }
 };
