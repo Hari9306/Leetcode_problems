569b012dbefbca35045e42435caf74122e5b1945
Hari9306 <hariveera9306@gmail.com>
2026-09-20 23:05:05 +0530

    Time: 0 ms (100%), Space: 9.6 MB (62.3%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -3,7 +3,7 @@ public:
     void gameOfLife(vector<vector<int>>& board) {
         int n=board.size();
         int m=board[0].size();
-        vector<vector<int>> cmp(n,vector<int>(m,0));
+        // vector<vector<int>> cmp(n,vector<int>(m,0));
         int dx[8]={-1,-1,-1,0,0,1,1,1};
         int dy[8]={-1,0,1,-1,1,-1,0,1};
         for(int i=0;i<n;i++){
@@ -12,21 +12,24 @@ public:
                 for(int k=0;k<8;k++){
                     int nr=i+dx[k];
                     int nc=j+dy[k];
-                    if(nr >=0 && nc>=0 && nr<n && nc<m && board[nr][nc]==1){
+                    if(nr >=0 && nc>=0 && nr<n && nc<m && (board[nr][nc]==1  || board[nr][nc]==3)){
                         live++;
                     }
                 }
                 if(board[i][j]){
-                    if(live>=2 && live<=3) cmp[i][j]=1;
-                    else cmp[i][j]=0;
+                    if(live>=2 && live<=3) board[i][j]=3;
                 }
                 else{ 
-                    if(live==3)cmp[i][j]=1;
-                    else cmp[i][j]=0;
+                    if(live==3)board[i][j]=2;
                 }
             }
         }
-        board=cmp;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==2 || board[i][j]==3) board[i][j]=1;
+                else board[i][j]=0;
+            }
+        }
     }
 };
 // less than  2 is dies
