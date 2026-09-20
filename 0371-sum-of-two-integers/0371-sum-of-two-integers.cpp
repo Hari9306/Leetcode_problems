fb0d8982aee648c97e90a8898ca96c78dcaf94fc
Hari9306 <hariveera9306@gmail.com>
2026-09-20 23:31:48 +0530

    Time: 0 ms (100%), Space: 7.8 MB (34.18%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-      int sum=a^b;
-      int carry=(a&b)<<1;
-      while(carry!=0){
-        a=sum;
-        b=carry;
-        sum=a^b;
-        carry=(a&b)<<1;
-      }
-      return sum;
+        int sum=a^b;
+        int carry=(a&b)<<1;
+        while(carry!=0){
+            a=sum;
+            b=carry;
+            sum=a^b;
+            carry=(a&b)<<1;
+        }
+        return sum;
     }
 };
+// 01 
+// 10 
+// 11
+// 10 
+// 11 xor 
+// 01
+// 10
