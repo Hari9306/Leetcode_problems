cfb8d69d6b10db6e37519fcef9f386cdaa7e07ef
Hari9306 <hariveera9306@gmail.com>
2026-09-22 15:07:03 +0530

    Time: 1 ms (43.46%), Space: 103.8 MB (37.18%) - LeetHub

---
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        int maxi=INT_MIN;
+        int mini=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            maxi=max(nums[i],maxi);
+            mini=min(nums[i],mini);
+        }
+        return 1ll*k*(maxi-mini);
+    }
+};
